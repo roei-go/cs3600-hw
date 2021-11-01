4888fbab085a18a571e44bc06ee5c6d824f26492
moshe <moshekimhi91@gmail.com>
2021-11-01 20:56:16 +0200

    hw1

---
+++ hw1/hw1/answers.py
@@ -0,0 +1,64 @@
+r"""
+Use this module to write your answers to the questions in the notebook.
+
+Note: Inside the answer strings you can use Markdown format and also LaTeX
+math (delimited with $$).
+"""
+
+# ==============
+# Part 1 answers
+
+part1_q1 = r"""
+**Your answer:**
+"""
+
+part1_q2 = r"""
+**Your answer:**
+"""
+
+# ==============
+# Part 2 answers
+
+part2_q1 = r"""
+**Your answer:**
+"""
+
+part2_q2 = r"""
+**Your answer:**
+"""
+
+# ==============
+
+# ==============
+# Part 3 answers
+
+part3_q1 = r"""
+**Your answer:**
+"""
+
+part3_q2 = r"""
+**Your answer:**
+"""
+
+part3_q3 = r"""
+**Your answer:**
+"""
+
+# ==============
+
+# ==============
+# Part 4 answers
+
+part4_q1 = r"""
+**Your answer:**
+"""
+
+part4_q2 = r"""
+**Your answer:**
+"""
+
+part4_q3 = r"""
+**Your answer:**
+"""
+
+# ==============
